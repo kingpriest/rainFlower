0257276859b7b357e4c0bc93b4006c6c21f49e11
priestLau <kingpriest@163.com>
2018-05-31 23:11:08 +0800

    11
    111

--- product.server.js
+++ product.server.js
@@ -13,38 +13,38 @@ router.get('/', (req, res, next) => {
 });
 app.use(router);
 
-const appData = require('./data.json');
-var seller = appData.seller;
-var goods = appData.goods;
-var ratings = appData.ratings;
+// const appData = require('./data.json');
+// var seller = appData.seller;
+// var goods = appData.goods;
+// var ratings = appData.ratings;
 
-var apiRoutes = express.Router();
+// var apiRoutes = express.Router();
 
-app.get('/api', (req, res) => {
-    res.json({
-        errno: 0,
-        data: appData
-    })
-});
-app.get('/api/seller', (req, res) => {
-    res.json({
-        errno: 0,
-        data: seller
-    })
-});
-app.get('/api/goods', (req, res) => {
-    res.json({
-        errno: 0,
-        data: goods
-    })
-});
-app.get('/api/ratings', (req, res) => {
-    res.json({
-        errno: 0,
-        data: ratings
-    })
-});
-app.use('/api', apiRoutes);
+// app.get('/api', (req, res) => {
+//     res.json({
+//         errno: 0,
+//         data: appData
+//     })
+// });
+// app.get('/api/seller', (req, res) => {
+//     res.json({
+//         errno: 0,
+//         data: seller
+//     })
+// });
+// app.get('/api/goods', (req, res) => {
+//     res.json({
+//         errno: 0,
+//         data: goods
+//     })
+// });
+// app.get('/api/ratings', (req, res) => {
+//     res.json({
+//         errno: 0,
+//         data: ratings
+//     })
+// });
+// app.use('/api', apiRoutes);
 app.use(express.static('./dist'));
 module.exports = app.listen(port, function (err) {
     if (err) {
